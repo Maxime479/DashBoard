a05b8bacf66b074de50c65a34e8985c6836ad794
Maxime <maxime.saurin@gmail.com>
2021-10-20 15:42:21 +0200

    Aside - Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './css/App.css';
 import Container from "./js/Container";
 import React from "react";
+import './js/icons/FontAwesomeIcon';
 
 function App() {
   return (
---
+++ src/js/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export default class Navigation extends React.Component{
+    render(){
+        return(
+            <div className={"navigation " + this.props.className}>
+
+                <nav>
+
+                </nav>
+
+
+
+            </div>
+        )
+
+    }
+}
+
+
