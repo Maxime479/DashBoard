80539a68c8f71f2c0b7de8124be31d33f186d7db
Maxime <maxime.saurin@gmail.com>
2021-11-17 21:18:43 +0100

    Correct Routes bug, react dom v6 --> Switch = Routes et component = element

--- App/src/routes.js
+++ App/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 
 import Home from './js/routes/Home';
 
@@ -12,12 +12,12 @@ import NotFound from './js/routes/NotFound.js';
 
 
 export default () => (
-    <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms" exact component={Rooms} />
-        <Route path="/devices" exact component={Devices} />
-        <Route path="/statistics" exact component={Statistics} />
-        <Route path="/members" exact component={Members} />
-        <Route component={NotFound} />
-    </Switch>
+    <Routes>
+        <Route path="/" exact element={Home} />
+        <Route path="/rooms" exact element={Rooms} />
+        <Route path="/devices" exact element={Devices} />
+        <Route path="/statistics" exact element={Statistics} />
+        <Route path="/members" exact element={Members} />
+        <Route element={NotFound} />
+    </Routes>
 )
