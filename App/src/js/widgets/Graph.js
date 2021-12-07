dff99ae8e83081943cff0557cbde86d83ca5e455
Maxime <maxime.saurin@gmail.com>
2021-12-07 05:34:32 +0100

    Graph Innit

---
+++ App/src/js/widgets/Graph.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+import '../../css/Statistics.css';
+
+
+export default class Graph extends React.Component{
+    render(){
+        return(
+            <div className={"graph " + this.props.className}>
+
+
+
+
+            </div>
+        )
+    }
+}
+
+
