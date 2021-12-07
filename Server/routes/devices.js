59399b6f1d5a7ed79374468075be1e7d43e866db
Maxime <maxime.saurin@gmail.com>
2021-12-07 08:53:25 +0100

    Try to add put method for device state

--- Server/routes/devices.js
+++ Server/routes/devices.js
@@ -30,6 +30,61 @@ router.post('/', async (req,res) =>{
   }
 });
 
+router.put('/state', async (req,res) =>{
+
+
+  const id = req.body.id;
+  const state = req.body.state;
+
+  console.log("Data get");
+  console.log(id);
+  console.log(state);
+  console.log("Data get");
+
+
+  // try{
+
+    const device = await Device.find({_id: id});
+
+    console.log("Found");
+    console.log(device);
+    console.log("Found");
+
+
+    device.state = state;
+    console.log("mark1");
+
+    console.log("Modified");
+    console.log(device);
+    console.log("Modified");
+
+
+
+    Device.updateOne({_id: req.params.id}, device).then(
+        () => {
+          res.status(201).json({
+            message: 'Device updated successfully!'
+          });
+        }
+    ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+    );
+
+
+
+  //   res.status(200).json({
+  //     message: "state updated successfully",
+  //   });
+  // }catch (err){
+  //   res.status(400).json({message: err});
+  //   console.log("Change State Error")
+  // }
+});
+
 module.exports = router;
 
 
