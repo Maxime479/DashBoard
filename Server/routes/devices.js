72b1a159abd0566bec80ca7e723960192038363a
Theoph69 <theophiledu69@gmail.com>
2021-12-09 18:30:11 +0100

    Updated PUT routes for devices and users to be able to modify any attributes

--- Server/routes/devices.js
+++ Server/routes/devices.js
@@ -3,67 +3,78 @@ const router = express.Router();
 const Device = require("../models/device");
 const User = require("../models/user");
 
-router.get('/', async (req, res) =>{
-  try{
-    const device = await Device.find();
-    res.json(device);
-  }catch (e) {
-    res.json({message: e});
-  }
+router.get('/', async (req, res) => {
+    try {
+        const device = await Device.find();
+        res.json(device);
+    } catch (e) {
+        res.json({message: e});
+    }
 });
 
-router.post('/', async (req,res) =>{
-  const device = new Device({
-    name : req.body.name,
-    room: req.body.room,
-    type: req.body.type,
-    icon: req.body.icon,
-    data: req.body.data,
-    unit:req.body.unit,
-    stored_data: req.body.stored_data,
-    state: req.body.state
-  });
+router.post('/', async (req, res) => {
+    const device = new Device({
+        name: req.body.name,
+        room: req.body.room,
+        type: req.body.type,
+        icon: req.body.icon,
+        data: req.body.data,
+        unit: req.body.unit,
+        stored_data: req.body.stored_data,
+        state: req.body.state
+    });
 
-  try{
-    const savedDevices = await device.save();
-    const PushData = await device.stored_data.push(donne);
-    res.json(savedDevices);
-    res.json(PushData);
-  }catch (err){
-    res.json({message: err});
-  }
+    try {
+        const savedDevices = await device.save();
+        const PushData = await device.stored_data.push(donne);
+        res.json(savedDevices);
+        res.json(PushData);
+    } catch (err) {
+        res.json({message: err});
+    }
 });
 
 router.get('/:deviceID', async (req, res) => {
 
-  try {
-    const device = await Device.findById(req.params.deviceID);
+    try {
+        const device = await Device.findById(req.params.deviceID);
 
-    res.json(device);
-  }catch (err) {
-    res.json({message:err});
-  }
+        res.json(device);
+    } catch (err) {
+        res.json({message: err});
+    }
 });
 
 router.delete('/:deviceID', async (req, res) => {
-  try {
-    const removeDevice = await Device.remove({_id: req.params.deviceID});
-    res.json(removeDevice);
-  }catch (err) {
-    res.json({message:err});
-  }
+    try {
+        const removeDevice = await Device.remove({_id: req.params.deviceID});
+        res.json(removeDevice);
+    } catch (err) {
+        res.json({message: err});
+    }
 });
 
 router.put('/:deviceID', async (req, res) => {
-  try {
-    const updatedDevice = await Device.updateOne(
-        {_id: req.params.deviceID},
-        {$set: {state: req.body.state}}
-    );
-    res.json(updatedDevice);
-  }catch (err) {
-    res.json({message:err});
-  }
+    try {
+        const updatedDevice = await Device.updateOne(
+            {_id: req.params.deviceID},
+            {
+                $set: {
+                    name: req.body.name,
+                    room: req.body.room,
+                    type: req.body.type,
+                    icon: req.body.icon,
+                    data: req.body.data,
+                    unit: req.body.unit,
+                    stored_data: req.body.stored_data,
+                    state: req.body.state
+                }
+            }
+        );
+        res.json(updatedDevice);
+    } catch (err) {
+        res.json({message: err});
+    }
 });
 
 // router.put('/state', async (req,res) =>{
