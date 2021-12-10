44dbf86ff44bcbc09be777729c1429c0e0df7f90
Maxime <maxime.saurin@gmail.com>
2021-12-10 13:40:35 +0100

    Stats

--- App/src/js/routes/Statistics.js
+++ App/src/js/routes/Statistics.js
@@ -5,6 +5,7 @@ import Profil from "../cards/Profil";
 import Graph from "../widgets/Graph";
 import ObjectID from "bson-objectid";
 import Image from "../tools/Image";
+import axios from "axios";
 
 
 
@@ -16,6 +17,8 @@ export default class Statistics extends React.Component{
 
 
         this.state = {
+
+
 
             navStyles: {
                 none: {display: 'none'},
@@ -47,25 +50,74 @@ export default class Statistics extends React.Component{
     }
 
 
+
+
+    sortSensors = (devicesData) => {
+
+        let tempDevice = [];
+
+        for(let i=0; i<devicesData.length; i++){
+
+
+            if(devicesData[i].type.includes("sensor")){
+                tempDevice.push(devicesData[i])
+            }
+        }
+
+        return tempDevice
+    }
+
+    getDevicesData = () => {
+
+        axios.get('/devices')
+            .then(response => {
+                this.setState({devicesData: this.sortSensors(response.data)})
+            })
+    }
+
+    componentDidMount() {
+        this.getDevicesData()
+    }
+
+    componentDidUpdate(prevState) {
+
+        if(this.state.devicesData !== undefined){
+            if(prevState.devicesData !== this.state.devicesData){
+                this.getDevicesData()
+                // console.log("UPDATE")
+            }
+        }
+    }
+
+
+
+
     render() {
 
-        return(
-            <div className="App">
+        if (this.state.devicesData === undefined){
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
+        }else{
+            return(
+                <div className="App">
 
-                <Image
-                    src="https://www.pngrepo.com/png/311018/512/navigation.png"
-                    onClick={this.displayNav}
-                    className="navButton"
-                    alt="boutton de navigation"
-                />
+                    <Image
+                        src="https://www.pngrepo.com/png/311018/512/navigation.png"
+                        onClick={this.displayNav}
+                        className="navButton"
+                        alt="boutton de navigation"
+                    />
 
-                <header className="mainHeader">
-                    <h1>Smart Home DashBoard</h1>
-                </header>
+                    <header className="mainHeader">
+                        <h1>Smart Home DashBoard</h1>
+                    </header>
 
-                <body className="mainBody">
+                    <body className="mainBody">
 
-                <aside className="navContainer" style={this.state.navStyle}>
+                    <aside className="navContainer" style={this.state.navStyle}>
                         <Navigation
                             selected="statistics"
                         />
@@ -77,30 +129,55 @@ export default class Statistics extends React.Component{
 
                         <div className="upStats">
 
-                            <Long
-                                className="temp"
-                                title="Température"
-                                data="+25°C"
-                            />
+                            {this.state.devicesData.map(function(item, key) {
 
-                            <Long
-                                className="hum"
-                                title="Humidité"
-                                data="30%"
-                            />
+                                return (
 
-                            <Long
-                                className="lum"
-                                title="Luminosité"
-                                data="150lm"
-                            />
+                                    <div key ={key} className="longSensorCharts">
+                                        <Long
+                                            // className="temp"
+                                            title={item.name}
+                                            data={item.data}
+                                        />
 
+
+                                    </div>
+
+                                )
+
+                            })}
 
                         </div>
+
+
+
+                        {/*<div className="upStats">*/}
+
+                        {/*    <Long*/}
+                        {/*        className="temp"*/}
+                        {/*        title="Température"*/}
+                        {/*        data="+25°C"*/}
+                        {/*    />*/}
+
+                        {/*    <Long*/}
+                        {/*        className="hum"*/}
+                        {/*        title="Humidité"*/}
+                        {/*        data="30%"*/}
+                        {/*    />*/}
+
+                        {/*    <Long*/}
+                        {/*        className="lum"*/}
+                        {/*        title="Luminosité"*/}
+                        {/*        data="150lm"*/}
+                        {/*    />*/}
+
+
+                        {/*</div>*/}
 
                         <div className="downGraph">
 
                             <Graph
+                                caller={this.state.devicesData}
                             />
 
 
@@ -115,15 +192,15 @@ export default class Statistics extends React.Component{
                         />
                     </aside>
 
-                </body>
+                    </body>
 
 
 
 
 
-            </div>
+                </div>
+            )
+        }
 
-
-        )
     }
 }
