51a2d4897d461f8b7bf07901c1d72bbe7cd45a54
Maxime <maxime.saurin@gmail.com>
2021-12-12 01:48:23 +0100

    Tab Update

--- App/src/js/nav/AdminTab.js
+++ App/src/js/nav/AdminTab.js
@@ -1,11 +1,5 @@
 import React from "react";
-import Image from "../tools/Image";
-import Text from "../tools/Text"
-
-import '../../css/addons/Device.css';
-
 import axios from "axios";
-import Device from "./Device";
 
 
 export default class AdminTab extends React.Component{
@@ -21,7 +15,6 @@ export default class AdminTab extends React.Component{
 
         };
 
-        // this.handleChange = this.handleChange.bind(this);
     }
 
 
@@ -37,7 +30,6 @@ export default class AdminTab extends React.Component{
         // console.log("------------------------------------------")
     }
 
-
     getDevicesData = () => {
 
         let tempDevices = {}
@@ -49,14 +41,6 @@ export default class AdminTab extends React.Component{
                 console.log(response)
                 console.log(response.data)
 
-                // tempDevices = response.data
-                // delete tempDevices.stored_data
-                //
-                // for(let i=0; i<tempDevices.length; i++){
-                //     tempArray.push(Object.keys(tempDevices[i]))
-                // }
-                //
-                // this.setState({devicesData: tempArray})
                 this.setState({devicesData: response.data})
             })
     }
@@ -69,6 +53,107 @@ export default class AdminTab extends React.Component{
     }
 
 
+    compareTable = (type) => {
+
+        switch (type){
+            case 'name':
+                return(
+                    function compare( a, b ) {
+                        if ( a.name < b.name ){
+                            return -1;
+                        }
+                        if ( a.name > b.name ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            case 'state':
+                return(
+                    function compare( a, b ) {
+                        if ( a.state < b.state ){
+                            return -1;
+                        }
+                        if ( a.state > b.state ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            case 'data':
+                return(
+                    function compare( a, b ) {
+                        if ( a.data < b.data ){
+                            return -1;
+                        }
+                        if ( a.data > b.data ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            case 'id':
+                return(
+                    function compare( a, b ) {
+                        if ( a._id < b._id ){
+                            return -1;
+                        }
+                        if ( a._id > b._id ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            case 'unit':
+                return(
+                    function compare( a, b ) {
+                        if ( a.unit < b.unit ){
+                            return -1;
+                        }
+                        if ( a.unit > b.unit ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            case 'room':
+                return(
+                    function compare( a, b ) {
+                        if ( a.room < b.room ){
+                            return -1;
+                        }
+                        if ( a.room > b.room ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            case 'type':
+                return(
+                    function compare( a, b ) {
+                        if ( a.type < b.type ){
+                            return -1;
+                        }
+                        if ( a.type > b.type ){
+                            return 1;
+                        }
+                        return 0;
+                    }
+                )
+            default:
+                return(
+                    function compare()  {
+                        return 0;
+                    }
+                )
+
+        }
+
+    }
+
+    sortTable = (type) => {
+        this.setState({devicesData: this.state.devicesData.sort(this.compareTable(type))})
+    }
 
 
     componentDidMount() {
@@ -78,9 +163,12 @@ export default class AdminTab extends React.Component{
 
     componentDidUpdate(prevState) {
 
-        if((this.state.table === undefined) && (this.state.memberData !== undefined))
+        if((this.state.choosedTable === undefined) && (this.state.membersData !== undefined)){
+            console.log("Did it")
             this.setState({choosedTable: this.userTable(this.state.membersData)})
         }
+
+    }
 
 
     // sendDataInDB = () => {
@@ -160,16 +248,69 @@ export default class AdminTab extends React.Component{
 
     }
 
+    devicesTable = (devicesData) =>{
 
-    // showState = () => {
-    //
-    // }
-    //
+        return(<table>
+            <thead className="adminTabRow">
+            <tr>
+                <td className="iconHead">Icon</td>
+                <td onClick={() => {this.sortTable("name")}} >Nom</td>
+
+                <td onClick={() => {this.sortTable("data")}} >Valeur</td>
+                <td onClick={() => {this.sortTable("unit")}} >Unité</td>
+
+                <td onClick={() => {this.sortTable("state")}} >État</td>
+
+                <td onClick={() => {this.sortTable("room")}} >Pièce</td>
+                <td onClick={() => {this.sortTable("type")}} >Type</td>
+
+                <td onClick={() => {this.sortTable("id")}} >ID</td>
+            </tr>
+            </thead>
+            <tbody>
+            {devicesData.map(function(item, key) {
+
+                let state
+
+                if(item.state === true){
+                    state = <span className="showOn">ON</span>
+
+                }else{
+                    state = <span className="showOff">OFF</span>
+                }
+
+
+                return (
+                    <tr key = {key} className="adminTabRow">
+                        <td><img
+                            alt="device icon"
+                            src={item.icon}
+                            className="tableIcon"
+                        /></td>
+                        <td>{item.name}</td>
+
+                        <td>{item.data}</td>
+                        <td>{item.unit}</td>
+
+                        <td>{state}</td>
+
+                        <td>{item.room}</td>
+                        <td>{item.type}</td>
+
+                        <td>{item._id}</td>
+                    </tr>
+                )
+
+            })}</tbody>
+        </table>)
+
+    }
+
 
 
     render(){
 
-        if((this.state.devicesData === undefined) || (this.state.membersData === undefined) || (this.state.choosedTable !== undefined)){
+        if((this.state.devicesData === undefined) || (this.state.membersData === undefined) || (this.state.choosedTable === undefined)){
             console.log("EMPTY");
 
             return (
@@ -186,135 +327,21 @@ export default class AdminTab extends React.Component{
             let membersData = this.state.membersData
             let devicesData = this.state.devicesData
 
-            const resultArray = Object.keys(devicesData);
 
-            let deviceTable = <table>
-                <thead className="adminTabRow">
-                <tr>
-                    <td>ID</td>
+                    return (
 
-                    <td>Icon</td>
-                    <td>Nom</td>
-
-                    <td>État</td>
-
-                    <td>Valeur</td>
-                    <td>Unité</td>
-
-                    <td>Pièce</td>
-                    <td>Type</td>
-                    <td>rooms</td>
-                </tr>
-                </thead>
-                <tbody>
+                    <div className="AdminTabContainer">
 
 
-                {devicesData.map(function (item, key) {
 
-                    for (let i = 0; i < devicesData.length; i++) {
-
-                        let device = devicesData[i]
+                    {this.devicesTable(devicesData)}
+                    {this.userTable(membersData)}
 
 
-                        return (
-                            <tr key={key} className="adminTabRow">
-                                <td>{device._id}</td>
-
-                                <td><img
-                                    src={device.icon}
-                                    className="tableIcon"
-                                /></td>
-                                <td>{device.name}</td>
-                                <td>{device.room}</td>
-
-                                {/*<td>{item.state}</td>*/}
-
-                                <td>{device.data}</td>
-                                <td>{device.unit}</td>
-
-                                <td>{device.room}</td>
-                                <td>{device.type}</td>
-                            </tr>
-                        )
-
-                    }
-
-                    // return (
-                    //     <tr key = {key} className="adminTabRow">
-                    //         <td>{item._id}</td>
-                    //         <td><img
-                    //             src={item.icon}
-                    //             className="tableIcon"
-                    //         /></td>
-                    //         <td>{item.name}</td>
-                    //         <td>{item.room}</td>
-                    //
-                    //         {/*<td>{item.state}</td>*/}
-                    //
-                    //         <td>{item.data}</td>
-                    //         <td>{item.unit}</td>
-                    //
-                    //         <td>{item.room}</td>
-                    //         <td>{item.type}</td>
-                    //     </tr>
-                    // )
-
-                })}
-
-
-                </tbody>
-            </table>
-
-
-            return (
-
-                <div className="AdminTabContainer">
-
-                    {/*{this.state.choosedTable}*/}
-
-                    <table>
-                        <thead className="adminTabRow">
-                        <tr>
-                            <td>ID</td>
-                            <td>Privileges</td>
-                            <td>Prénom</td>
-                            <td>Nom</td>
-                            {/*<td>{new Date(item.birthDate)}</td>*/}
-                            <td>Login</td>
-                            <td>Mot de passe</td>
-                            {/*<td>{item.photo}</td>*/}
-                            <td>Appareils</td>
-                            <td>Pièces</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {membersData.map(function(item, key) {
-
-                            return (
-                                <tr key = {key} className="adminTabRow">
-                                    <td>{item._id}</td>
-                                    <td>{item.privileges}</td>
-                                    <td>{item.first_name}</td>
-                                    <td>{item.last_name}</td>
-                                    {/*<td>{new Date(item.birthDate)}</td>*/}
-                                    <td>{item.login}</td>
-                                    <td>{item.password}</td>
-                                    {/*<td>{item.photo}</td>*/}
-                                    <td>{item.devices}</td>
-                                    <td>{item.rooms}</td>
-                                </tr>
-                            )
-
-                        })}</tbody>
-                    </table>
-
-                </div>
-            )
+                    </div>
+                    )
 
         }
-
-
-
 
     }
 }
